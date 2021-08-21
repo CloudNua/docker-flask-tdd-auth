1f74955de8dac7a8b5cce8f733015619370e8bf7
Enda Kelly <kelly.enda@gmail.com>
2021-08-21 15:43:34 +0100

    new: react forms 1. Handle form submit 2. Obtain user input 3. Send AJAX request 4. Update / refresh page

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';  // new
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 
@@ -11,8 +13,14 @@ class App extends Component {
     super();
 
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '', 
     };
+
+    this.addUser = this.addUser.bind(this);
+
+    this.handleChange = this.handleChange.bind(this);
   };
 
   componentDidMount() {
@@ -25,15 +33,44 @@ class App extends Component {
     .catch((err) => { console.log(err); });
   }
 
+  addUser(event) {
+    event.preventDefault();
+  
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+  
+    axios.post(`${process.env.REACT_APP_API_SERVICE_URL}/users`, data)
+    .then((res) => {
+      this.getUsers();
+      this.setState({ username: '', email: '' });
+    })
+    .catch((err) => { console.log(err); });
+  };
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+
   render() {
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
               <br/>
               <h1 className="title is-1">Users</h1>
               <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                addUser={this.addUser}
+                // eslint-disable-next-line react/jsx-handler-names
+                handleChange={this.handleChange}
+              />
+              <br/><br/>
               <UsersList users={this.state.users}/>
             </div>
           </div>
